481c9238c10f11132ad307fc22ab8f8618c92423
Julius Pedersen <deifyed@gmail.com>
2020-02-12 21:59:47 +0100

    IMPROVE: package structure final v3

--- makefile
+++ makefile
@@ -2,6 +2,7 @@
 
 build:
 	npm run build
+	cp package.json dist/package.json
 
 clean:
 	@rm -r dist/ || true
--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,6 @@ module.exports = {
   context: path.resolve(__dirname, 'src'),
 
   entry: {
-    'package.json': '../package.json',
   	models: './models.js'
   },
 
